a19dfe3ef2a52e876ab4509408c390f17fcbeff2
Morten Enemark Lund <melund@gmail.com>
2016-09-10 16:46:55 +0200

    Register the pytest manually, if setuptools is not available.

--- conftest.py
+++ conftest.py
@@ -1,2 +1,10 @@
 # empty file to trick py.test into adding the root folder to sys.path
 # see https://github.com/pytest-dev/pytest/issues/911 for more info
+
+try:
+    import setuptools
+except ImportError:
+    # if setuptools is not available we register
+    # the plugin manually.
+    pytest_plugins = "xonsh.pytest_plugin",
+
